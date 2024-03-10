b9bed5836abc9c2068df6f635d1fecc694013c16
swapnilsudrik <113012667+swapnilsudrik@users.noreply.github.com>
2024-03-10 20:29:14 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -203,7 +203,7 @@ function showProjects(projects) {
 
         projectHTML += `
         <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="./assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
